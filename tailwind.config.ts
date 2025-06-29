56acee4155c68ed32885c02c2bf6a66aa87c5e1a
llddang <laliddang@gmail.com>
2025-06-29 16:52:58 +0900

    setting:#3 색상 theme 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: '#171717',
+        gray: {
+          '10': '#171717',
+          '20': '#3d3d3d',
+          '30': '#5d5d5d',
+          '40': '#888888',
+          '50': '#fafafa',
+        },
+        error: '#FF7A7C',
+        semantic: '#FF3F62',
       },
     },
   },
