c9ecd4224a6e711542556933a008769c24962753
llddang <laliddang@gmail.com>
2025-07-27 01:14:05 +0900

    chore:#5 index.ts도 자동으로 참조 가능하도록 next 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: config => {
+    config.resolve.extensions = ['.ts', '.tsx', '.js', '.jsx', ...config.resolve.extensions];
+    return config;
+  },
+};
 
 export default nextConfig;
