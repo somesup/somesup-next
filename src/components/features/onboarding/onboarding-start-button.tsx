bb8542f40a11e8f03c3f1f70b17bbe0a481588a9
llddang <laliddang@gmail.com>
2025-07-27 14:30:44 +0900

    docs: 게스트 로그인 버튼에 todo 추가

--- src/components/features/onboarding/onboarding-start-button.tsx
+++ src/components/features/onboarding/onboarding-start-button.tsx
@@ -4,12 +4,18 @@ import { Button, buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 
 const OnboardingStartButton = () => {
+  const handleGuestSignIn = () => {
+    // TODO: API 연결
+  };
+
   return (
     <div className="absolute top-0 flex h-screen w-full flex-col justify-end gap-2 px-8 pb-10">
       <Link href="/sign-in" className={buttonVariants({ variant: 'default', size: 'default' })}>
         전화번호로 시작하기
       </Link>
-      <Button variant="secondary">게스트로 시작하기</Button>
+      <Button variant="secondary" onClick={handleGuestSignIn}>
+        게스트로 시작하기
+      </Button>
     </div>
   );
 };
