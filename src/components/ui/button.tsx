542833bb554b107f4f570c23ea6a470f75e5e4b0
llddang <laliddang@gmail.com>
2025-08-02 22:09:40 +0900

    feat:#8 ghost 추가

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,15 +3,17 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  'inline-flex items-center typography-body1 justify-center whitespace-nowrap rounded-[0.625rem] transition-all disabled:pointer-events-none disabled:opacity-50 shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
+  'inline-flex px-2 py-1 items-center typography-body1 justify-center whitespace-nowrap rounded-[0.625rem] transition-all disabled:pointer-events-none disabled:opacity-50 shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 aria-invalid:border-destructive',
   {
     variants: {
       variant: {
         default: 'bg-gray-60 text-gray-10 hover:bg-gray-60/80 active:bg-gray-60/40',
         secondary: 'bg-gray-20 text-gray-60 hover:bg-gray-20/80 active:bg-gray-20/40',
+        ghost: 'active:bg-gray-60/20',
       },
       size: {
         default: 'h-[3.75rem] w-full',
+        fit: 'h-fit w-fit',
       },
     },
     defaultVariants: {
