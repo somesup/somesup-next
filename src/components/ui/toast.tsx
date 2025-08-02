6af2c8f6433373cad7d5d56fd5510286f535713d
llddang <laliddang@gmail.com>
2025-08-02 20:18:35 +0900

    feat:#8 toast 컴포넌트 구현

---
+++ src/components/ui/toast.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { MdError } from 'react-icons/md';
+import { Toast, useToastStore } from '@/lib/stores/toast';
+
+const toastIcon: Record<Toast['type'], ReactNode> = {
+  success: <MdError />,
+  error: <MdError className="h-7 w-7 text-error" />,
+  info: <MdError />,
+};
+
+export const ToastContainer = () => {
+  const toasts = useToastStore(state => state.toasts);
+
+  return (
+    <div className="fixed bottom-0 left-1/2 z-50 flex w-full -translate-x-1/2 flex-col gap-4 p-4 sm:left-auto sm:right-0 sm:top-auto sm:w-[360px] sm:translate-x-0">
+      {toasts.map(toast => (
+        <ToastItem key={toast.id} {...toast} />
+      ))}
+    </div>
+  );
+};
+
+export const ToastItem = ({ title, description, type }: Toast) => {
+  return (
+    <div className="flex h-[4rem] w-full items-center gap-5 rounded-lg bg-gray-20 px-4">
+      {toastIcon[type]}
+      <div className="flex h-fit flex-col">
+        <p className="typography-body3">{title}</p>
+        <span className="typography-caption2">{description}</span>
+      </div>
+    </div>
+  );
+};
+
+const createToast = (type: Toast['type']) => (title: string, description: string) => {
+  const id = crypto.randomUUID();
+  useToastStore.getState().add({ type, title, description, id });
+  setTimeout(() => useToastStore.getState().remove(id), 3000);
+};
+
+export const toast = {
+  success: createToast('success'),
+  info: createToast('info'),
+  error: createToast('error'),
+  serverError: () => createToast('error')('서버에 문제가 발생했습니다.', '잠시후 다시 시도해주세요'),
+};
