629cf2a31e89012278f1e26d8d21a16d3691dc8d
llddang <laliddang@gmail.com>
2025-08-11 23:47:49 +0900

    feat:#11 로티 파일 위치 수정

--- src/components/features/set-preference/set-preference-lottie.tsx
+++ src/components/features/set-preference/set-preference-lottie.tsx
@@ -1,7 +1,7 @@
 import { Player } from '@lottiefiles/react-lottie-player';
 
 const SetPreferenceLottie = () => {
-  return <Player autoplay loop src="/animations/loading.json" className="aspect-auto w-full" />;
+  return <Player autoplay loop src="/animations/loading.json" className="aspect-auto w-[70%]" />;
 };
 
 export default SetPreferenceLottie;
--- src/components/features/set-preference/set-preference-wait-page.tsx
+++ src/components/features/set-preference/set-preference-wait-page.tsx
@@ -7,11 +7,13 @@ const SetPreferenceWaitPage = () => {
   const nickname = useUserStore(state => state.user.nickname);
 
   return (
-    <section className="px-8 pb-4 pt-24 text-center">
+    <section className="flex h-full flex-col px-8 pb-32 pt-24 text-center">
       <h3 className="!font-bold typography-small-title">
         {nickname} 님을 위한 {nickname.length > 6 && <br />} 뉴스를 구성하고 있어요
       </h3>
-      <SetPreferenceLottie />
+      <div className="flex flex-1 items-center">
+        <SetPreferenceLottie />
+      </div>
     </section>
   );
 };
