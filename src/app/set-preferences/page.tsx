ed8fce0003572cb5655f9ad0e76701a67ca48587
llddang <laliddang@gmail.com>
2025-08-11 23:37:19 +0900

    feat:#11 선호도 설정 페이지 구현

---
+++ src/app/set-preferences/page.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+import SetPreferenceFirstPage from '@/components/features/set-preference/set-preference-first-page';
+import SetPreferenceDetailPage from '@/components/features/set-preference/set-preference-detail-page';
+import SetPreferenceFinishPage from '@/components/features/set-preference/set-preference-finish-page';
+import { useUserStore } from '@/lib/stores/user';
+import { sectionLabels, sectionMappingId } from '@/types/types';
+import { UpdatePreferencesRequestDto } from '@/types/dto';
+import { authUpdatePreferences } from '@/lib/apis/apis';
+import { toast } from '@/components/ui/toast';
+import SetPreferenceWaitPage from '@/components/features/set-preference/set-preference-wait-page';
+
+const SetPreferencesPage = () => {
+  const [step, setStep] = useState<'first' | 'detail' | 'finish' | 'wait'>('first');
+  const [isInitialSetup, setIsInitialSetup] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsInitialSetup(document.referrer === '/set-nickname');
+  }, []);
+
+  const handlePreferenceDone = async () => {
+    setStep('wait');
+
+    const preferences = useUserStore.getState().preferences;
+    const request = sectionLabels.reduce((acc, label) => {
+      acc.push({ sectionId: sectionMappingId[label], preference: preferences[label] });
+      return acc;
+    }, [] as UpdatePreferencesRequestDto);
+
+    const { error } = await authUpdatePreferences(request);
+    if (!error) return router.push('/');
+    toast.serverError();
+  };
+
+  return (
+    <main className="flex h-screen flex-col">
+      {step !== 'wait' && (
+        <header className="px-8 pt-6">
+          <div className="relative">
+            <button
+              onClick={() => router.push(isInitialSetup ? '/set-nickname' : '/mypage')}
+              className="absolute left-0 top-1/2 -translate-y-1/2"
+            >
+              <MdKeyboardArrowLeft size={29} />
+            </button>
+            <h2 className="text-center typography-small-title">맞춤 설정</h2>
+          </div>
+        </header>
+      )}
+      {step === 'first' && <SetPreferenceFirstPage onConfirm={() => setStep('detail')} />}
+      {step === 'detail' && <SetPreferenceDetailPage onConfirm={() => setStep('finish')} />}
+      {step === 'finish' && (
+        <SetPreferenceFinishPage onReSetup={() => setStep('detail')} onConfirm={handlePreferenceDone} />
+      )}
+      {step === 'wait' && <SetPreferenceWaitPage />}
+    </main>
+  );
+};
+
+export default SetPreferencesPage;
