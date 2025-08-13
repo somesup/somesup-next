a7e86f9fbecb33d337b01c8330c031549f72032e
llddang <laliddang@gmail.com>
2025-08-13 18:44:21 +0900

    style:#15 detail 변경

--- src/components/features/news/news-detail-view.tsx
+++ src/components/features/news/news-detail-view.tsx
@@ -5,12 +5,14 @@ type NewsDetailViewProps = Pick<NewsDto, 'fullSummary'>;
 const NewsDetailView = ({ fullSummary }: NewsDetailViewProps) => {
   return (
     <section className="relative h-full w-full flex-shrink-0">
-      <div className="absolute inset-0 -z-10 bg-black/60 backdrop-blur-3xl" />
+      <div className="absolute inset-0 -z-10 bg-black/30 backdrop-blur-3xl" />
       <div
         className="prose prose-sm prose-invert h-full overflow-y-auto px-7 py-8"
         style={{ touchAction: 'pan-y pinch-zoom', overscrollBehavior: 'contain' }}
       >
-        <ReactMarkdown components={{ p: props => <p className="text-base leading-8 tracking-tight" {...props} /> }}>
+        <ReactMarkdown
+          components={{ p: props => <p className="text-base leading-8 tracking-tight text-gray-60" {...props} /> }}
+        >
           {fullSummary}
         </ReactMarkdown>
       </div>
