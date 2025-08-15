a332f96ffb9583a2f4999ff8cc27113ce9b4b190
llddang <laliddang@gmail.com>
2025-08-15 22:17:19 +0900

    feat:#21 스크랩한 기사 보는 페이지 구현

---
+++ src/app/scrap/[scrapId]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import NewsCard from '@/components/features/news/news-card';
+import { getArticle } from '@/lib/apis/apis';
+import { NewsDto } from '@/types/dto';
+import { useEffect, useState } from 'react';
+import { MdKeyboardArrowLeft } from 'react-icons/md';
+
+const ScrapPage = ({ params }: { params: { scrapId: string } }) => {
+  const [article, setArticle] = useState<null | NewsDto>(null);
+  const [currentView, setCurrentView] = useState<'abstract' | 'detail'>('abstract');
+
+  useEffect(() => {
+    const get = async () => {
+      const { error, data } = await getArticle(Number(params.scrapId));
+      console.log(article, error);
+      if (!error) setArticle(data);
+    };
+    get();
+  }, []);
+
+  if (!article) return;
+  return (
+    <div>
+      <div
+        className={`fixed left-0 right-0 top-5 z-50 flex justify-center ${currentView === 'abstract' ? 'opacity-100' : 'opacity-0'}`}
+      >
+        <a href="/my-page/scrap" className="absolute left-4 top-1/2 -translate-y-1/2">
+          <MdKeyboardArrowLeft size={28} />
+        </a>
+        <span>스크랩 기사</span>
+      </div>
+      <NewsCard news={article} active onViewChange={setCurrentView} />
+    </div>
+  );
+};
+
+export default ScrapPage;
