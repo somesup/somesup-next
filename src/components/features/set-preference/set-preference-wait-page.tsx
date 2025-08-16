ea7e5c3393bfd302ba7caeda41d54752c7761856
llddang <laliddang@gmail.com>
2025-08-16 15:58:33 +0900

    feat:#27 선호도 설정 페이지의 마지막에서 진행에 따라 문구가 변경되도록 구현

--- src/components/features/set-preference/set-preference-wait-page.tsx
+++ src/components/features/set-preference/set-preference-wait-page.tsx
@@ -2,14 +2,34 @@
 
 import { useUserStore } from '@/lib/stores/user';
 import SetPreferenceLottie from './set-preference-lottie';
+import { ReactNode, useState } from 'react';
+
+const comments = [
+  (nickname: string) => (
+    <>
+      {nickname} 님을 위한
+      <br />
+      뉴스를 수집하고 있어요
+    </>
+  ),
+  () => '맞춤형 뉴스 피드를 구성하고 있어요',
+  () => '거의 다 되었어요 !',
+];
 
 const SetPreferenceWaitPage = () => {
   const nickname = useUserStore(state => state.user.nickname);
+  const [index, setIndex] = useState(0);
+
+  const timeId = setInterval(() => {
+    if (index >= comments.length - 1) return clearInterval(timeId);
+
+    setIndex(index + 1);
+  }, 2000);
 
   return (
-    <section className="flex h-full flex-col px-8 pb-32 pt-24 text-center">
-      <h3 className="!font-bold typography-small-title">
-        {nickname} 님을 위한 {nickname.length > 6 && <br />} 뉴스를 구성하고 있어요
+    <section className="relative flex h-full flex-col px-8 pb-32 pt-24 text-center">
+      <h3 className="absolute left-0 top-24 w-full text-center !font-bold typography-small-title">
+        {comments[index](nickname)}
       </h3>
       <div className="flex flex-1 items-center">
         <SetPreferenceLottie />
