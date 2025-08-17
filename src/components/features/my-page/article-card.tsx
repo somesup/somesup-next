89e214aa762031a814e2b84631b6204a866205c7
llddang <laliddang@gmail.com>
2025-08-18 08:51:46 +0900

    style:#32 스크랩 목록에서 타이틀이 gradient위에 있도록 수정

--- src/components/features/my-page/article-card.tsx
+++ src/components/features/my-page/article-card.tsx
@@ -5,8 +5,9 @@ const ArticleCard = ({ article, index }: { article: NewsDto; index: number }) =>
     <a href={`/scrap?index=${index}`} className="relative block overflow-hidden rounded-xl">
       <div
         style={{ backgroundImage: `url(${article.thumbnailUrl})` }}
-        className="relative aspect-[10/16] w-full bg-cover bg-center bg-no-repeat after:absolute after:inset-0 after:bg-[linear-gradient(to_top,theme(colors.gray.10)_0%,theme(colors.gray.10/0.4)_43%,transparent_64%,transparent_100%)] after:content-['']"
+        className="relative aspect-[10/16] w-full bg-cover bg-center bg-no-repeat"
       >
+        <div className="absolute inset-0 bg-[linear-gradient(to_top,theme(colors.gray.10)_0%,theme(colors.gray.10/0.4)_43%,transparent_64%,transparent_100%)]" />
         <div className="pointer-events-none absolute inset-x-0 bottom-0 p-4">
           <p className="break-keep leading-snug typography-caption2">{article.title}</p>
         </div>
